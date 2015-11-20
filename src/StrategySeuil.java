08f7f98872723450431dc52ecd68b6c45c05a13e
FredWe <fred@FredWetekiMacBook-Air.local>
2015-11-20 18:39:34 +0100

    update StrategyHumain & StrategySeuil

--- src/StrategySeuil.java
+++ src/StrategySeuil.java
@@ -1,18 +1,18 @@
 public class StrategySeuil implements Strategy {
 
-  public Integer seuil;
-
+  private Integer seuil;
 
   public void setSeuil(Integer s) {
-
+	  this.seuil = s;
   }
-
+  public Integer getSeuil(){
+	  return this.seuil;
+  }
 
 
 	@Override
 	public boolean deciderDemanderCarte(Integer somme) {
-		// TODO Auto-generated method stub
-		return false;
+		return (somme < this.seuil) ? true : false;
 	}
 
 }
